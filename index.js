0c75bb819538c11f51f9fc65eef14b65269cd1cf
OpenByte <development.openbyte@gmail.com>
2018-08-29 08:48:32 +0200

    Rename function

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ const path = require('path');
 const fs = require('fs');
 const afs = fs.promises;
 
-async function deleteFolderRecursive(directory, removeDirectory=true) {
+async function rmdirRecursiveAsync(directory, removeDirectory=true) {
     if (!fs.existsSync(directory))
 		return;
 	for (let entry of await afs.readdir(directory)) {
 		const currentPath = path.join(directory, entry);
 		if ((await afs.lstat(currentPath)).isDirectory())
-			await deleteFolderRecursive(currentPath);
+			await rmdirRecursiveAsync(currentPath);
 		else await afs.unlink(currentPath);
 	}
 	if (removeDirectory)
 		await afs.rmdir(directory);
 }
 
-module.exports = deleteFolderRecursive;
+module.exports = rmdirRecursiveAsync;
